ddb6d12b4cf69f5f760750cfa5fd209095e88487
Roy Wu <roy32011@gmail.com>
2023-12-06 21:08:46 +0800

    Day 26: Styling

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_number}", align="L", ln=1)
     pdf.cell(w=50, h=8, txt=f"Date: {date}", align="L", ln=1)
-
+    pdf.cell(w=50, h=8, ln=1)
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     # Add a header
@@ -24,9 +24,9 @@ for filepath in filepaths:
     columns = [item.replace("_", " ").title() for item in columns]
     pdf.set_font(family="Times", size=10, style="B")
     pdf.set_text_color(80, 80, 80)
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=30, h=8, txt=columns[0].replace("Id", "ID"), border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2][:6], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
@@ -49,6 +49,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt="", border=1)
     pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
+    pdf.cell(w=0, h=10, ln=1)
     # Add total sum sentence
     pdf.set_font(family="Times", size=10, style="B")
     pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
